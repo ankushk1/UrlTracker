20fa770822c249f507a84ac94c3966b736d9649c
ankushk1 <40850518+ankushk1@users.noreply.github.com>
2020-12-29 00:42:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func test(w http.ResponseWriter, r *http.Request){
 
 func main() {
     http.HandleFunc("/", test)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":20101", nil)
 }
